dd860cf24f969ed426be413b89098a6a03e53f7f
Kade Bolen <kadebolen@u.boisestate.edu>
2025-04-26 23:47:30 -0600

    Updated App.jsx to use HashRouter instead of BrowserRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,7 +9,7 @@ import NotFound from './pages/NotFound';
 
 function App() {
     return (
-        <Router basename={"/"}>
+        <Router basename={"/kabolen.github.io"}>
             <Header />
             <main>
                 <Routes>
