856c6b8200152c5f469515354e5028ff2b518843
Kade Bolen <kadebolen@u.boisestate.edu>
2025-04-27 00:16:39 -0600

    Added simple animations between pages.

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,30 @@ import Projects from './pages/Projects';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
+import { AnimatePresence} from "framer-motion";
+
+function AnimatedRoutes() {
+    const location = useLocation();
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </AnimatePresence>
+    );
+}
 
 function App() {
     return (
         <>
             <Header />
             <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
+                <AnimatedRoutes />
             </main>
             <Footer />
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
+import {motion} from 'framer-motion';
+
 function Home() {
     return (
-        <section>
+        <motion.section
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.5}}
+        >
             <h2>Welcome to My Portfolio!</h2>
             <p>I'm a Computer Science student passionate about technology, creativity, collaborating with others to
-            create impactful things.</p>
-        </section>
+                create impactful things.</p>
+        </motion.section>
     );
 }
 
