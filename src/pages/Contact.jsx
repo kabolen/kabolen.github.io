6149e3f0b97c926b78938e1de93adc4d2e4f70c9
Kade Bolen <kadebolen@u.boisestate.edu>
2025-04-29 15:01:00 -0600

    Update contact page to include functioning links. Changed font size.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,9 +8,10 @@ function Contact() {
             exit={{opacity: 0, scale: 0.95}}
             transition={{duration: 0.5}}
         >
-            <h2>Contact</h2>
-            <p>Email me at: <a href="mailto:kadesbolen@gmail.com">kadesbolen@gmail.com</a></p>
-            <p>Find me on <a href="https://linkedin.com/in/kabolen" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+            <h1>Contact</h1>
+            <h5>Email me at: <a href="mailto:kadesbolen@gmail.com">kadesbolen@gmail.com</a></h5>
+            <h5>Find me on <a href="https://www.linkedin.com/in/kade-bolen-65931a253/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h5>
+            <h5>Visit my <a href="https://github.com/kabolen" target="_blank" rel="noopener noreferrer">GitHub Profile</a></h5>
         </motion.section>
     );
 }
