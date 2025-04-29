5fe934e9e2aa0621457f6ac887c5152fc2a28dbf
Kade Bolen <kadebolen@u.boisestate.edu>
2025-04-29 15:02:06 -0600

    Removed "Kade Bolen" in favor of a small image (which is bugged).

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header>
-            <h1>Kade Bolen</h1>
+            <div className={"logo"}>
+                <img src={"../public/favicon.ico"} alt={"Kade Bolen Logo"} />
+                <span className={"site-name"}>Kade Bolen Portfolio</span>
+            </div>
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/projects">Projects</Link>
-                <Link to="/about">About</Link>
+                <Link to="/about">About Me</Link>
                 <Link to="/contact">Contact</Link>
             </nav>
         </header>
