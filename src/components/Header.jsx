4af14b37bd33ccd83a993a16fd017a9a34608cc8
Kade Bolen <kadebolen@u.boisestate.edu>
2025-05-02 20:51:51 -0600

    Added basic header bar.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div className='color-cycle' style={{width: '100%',
+            height: '2.8125rem',
+            background: 'var(--accent-color)',
+            border: '1px var(--stroke-color) solid',
+            filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+            marginBottom: '1.5rem'}}
+        />
+    )
+}
+
+export default Header;
