e4ebc6eae16a29d5ec4b2d1e6a122366adbea82c
Kade Bolen <kadebolen@u.boisestate.edu>
2025-05-10 11:22:46 -0600

    Moved divider from index.css to Divider.jsx

---
+++ src/components/Divider.jsx
@@ -0,0 +1,12 @@
+function Divider() {
+    return (
+        <div className='color-cycle' style={{
+            border: 'none',
+            borderTop: '1px solid var(--stroke-color)',
+            width: '35%',
+            marginTop: '1.5rem'}}
+        />
+    )
+}
+
+export default Divider;
