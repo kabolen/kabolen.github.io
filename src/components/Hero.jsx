b11a8188a4248de3b108d0d4a199fa37a2a5e57c
Kade Bolen <kadebolen@u.boisestate.edu>
2025-05-10 11:23:29 -0600

    Moved hero styling from index.css to Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,20 @@
-function Hero() {
+function Hero({children}) {
+    return (
+        <div style={{
+            width: '62%',
+            height: '13%',
+            minWidth: '31.5rem',
+            minHeight: '6rem',
+            borderRadius: '1.5625rem',
+            border: '1px solid var(--stroke-color)',
+            background: 'var(--accent-color)',
+            filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+            color: 'var(--body-color)',
+            alignContent: 'center',
+            flexShrink: 0}}>
+            {children}
+        </div>
+    )
 }
 
 export default Hero;
